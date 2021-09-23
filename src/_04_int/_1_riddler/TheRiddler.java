7ba9ddf2ae1a4f2085e7d447acc60f44ab0efc87
YeetMaster467 <elicomixinc@gmail.com>
2021-09-22 19:55:00 -0700

    Completed riddler
    Completed Robot in Space

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,17 +1,53 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
 
 
 public class TheRiddler {
-
+	
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		
+		int score = 0;
+		
+		String answer = JOptionPane.showInputDialog("What runs all the time but has no legs?");
+		if(answer.equalsIgnoreCase("code")) {
+			JOptionPane.showMessageDialog(null, "Yep!");
+			score += 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Nope, it was code!");
+		}
+		answer = JOptionPane.showInputDialog("What has to be broken before you use it?");
+		if(answer.equalsIgnoreCase("egg") || answer.equalsIgnoreCase("an egg")) {
+			JOptionPane.showMessageDialog(null, "Yep!");
+			score += 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Nope, it was an egg!");
+		}
+		answer = JOptionPane.showInputDialog("What has a bottom at its top?");
+		if(answer.equalsIgnoreCase("legs")) {
+			JOptionPane.showMessageDialog(null, "Yep!");
+			score += 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Nope, it was legs!");
+		}
+		answer = JOptionPane.showInputDialog("What is made of water but if it goes in water it dies?");
+		if(answer.equalsIgnoreCase("ice") || answer.equalsIgnoreCase("ice cubes") || answer.equalsIgnoreCase("an ice cube")) {
+			JOptionPane.showMessageDialog(null, "Yep!");
+			score += 1;
+		}else {
+			JOptionPane.showMessageDialog(null, "Nope, it was an ice cube!");
+		}
+		JOptionPane.showMessageDialog(null, "That's all the riddles for today!\nYou got a score of " + score + "!");
+		
+		
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -34,13 +34,25 @@ public class RobotInSpace extends KeyboardAdapter {
 		// each arrow key
 		
 		// 3. If the up arrow is pressed, move the Robot up the screen.
-		
+		if(keyPressed==38) {
+			rob.setAngle(0);
+			rob.move(10);
+		}
 		// 4. If the down arrow is pressed, move the Robot down.
-		
+		if(keyPressed==40) {
+			rob.setAngle(180);
+			rob.move(10);
+		}
 		// 5. If the left arrow is pressed, make the Robot go left.
-		
+		if(keyPressed==37) {
+			rob.setAngle(270);
+			rob.move(10);
+		}
 		// 6. If right is pressed, move the Robot right.
-		
+		if(keyPressed==39) {
+			rob.setAngle(90);
+			rob.move(10);
+		}
 		// 7. Run your program and move the Robot to R2-D2 for a surprise!
 		
 	}
